887b8530ceb1f75dbc22566658abea4c78491ee8
mnn <Matveih1@yandex.ru>
2020-10-26 21:13:15 +0300

    Подключить JS. Сделать сайт адаптивным

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,8 @@ let closeButton = document.querySelector('.popup__button-close');
 
 let infoName = document.querySelector('.profile__info-name');
 let nameInput = popupForm.querySelector('.popup__name-input');
+let infoDescription = document.querySelector('.profile__info-description');
+let descriptionInput = popupForm.querySelector('.popup__description-input');
 
 // найдем форму
 let formElement = popupForm.querySelector('.popup__container');
@@ -15,6 +17,7 @@ function editButtonClick() {
   popupForm.classList.add('popup_open');
 
   nameInput.value = infoName.textContent;
+  descriptionInput.value = infoDescription.textContent;
 }
 
 function closeButtonClick() {
@@ -27,7 +30,10 @@ function formSubmitHandler (evt) {
 
   // присвоем значение
   infoName.textContent = nameInput.value;
+  infoDescription.textContent = descriptionInput.value;
 
+  //закроем форму
+  closeButtonClick();
 }
 
 editButton.addEventListener('click', editButtonClick); 
