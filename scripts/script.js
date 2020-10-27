05d49725e9b8bf5908bd15868bac984090bc4d61
mnn <Matveih1@yandex.ru>
2020-10-27 09:33:50 +0300

    Отработать замечания ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 let popupForm = document.querySelector('.popup');
 
-let editButton = document.querySelector('.edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__button-close');
 
 let infoName = document.querySelector('.profile__info-name');
-let nameInput = popupForm.querySelector('.popup__name-input');
+let nameInput = popupForm.querySelector('input[name="name"]');
 let infoDescription = document.querySelector('.profile__info-description');
-let descriptionInput = popupForm.querySelector('.popup__description-input');
+let descriptionInput = popupForm.querySelector('input[name="description"]');
 
 // найдем форму
 let formElement = popupForm.querySelector('.popup__container');
